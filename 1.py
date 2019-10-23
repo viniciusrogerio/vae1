72ede1ab201801be9c5086535aca2d02a4e10f44
Vinicius Rogério da Silva <50369912+viniciusrogerio@users.noreply.github.com>
2019-10-23 18:35:58 -0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+def produtorio(lista):
+    resultado = 1 
+    for i in lista:
+        resultado = resultado*i 
+    return resultado
+def somatorio(lista):
+    resultado = 0
+    for i in lista:
+        resultado = resultado+i
+    return resultado
